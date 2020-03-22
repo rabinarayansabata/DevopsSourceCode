4776d2e471a131b714254138b11a1b5985209345
rabi <sabat.rabi46@gmail.com>
2020-03-22 19:59:09 +0530

    add Hello code

---
+++ Hello/src/main/java/myPkg/EntryClass.java
@@ -0,0 +1,26 @@
+package myPkg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@EnableAutoConfiguration
+@SpringBootApplication
+public class EntryClass {
+	
+	@RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(EntryClass.class, args);
+
+	}
+
+}
